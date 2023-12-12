e1db5d41a8366380461b38d6450c857e4a5d6285
seunghwan <lovelyunsh@naver.com>
2023-12-12 09:52:10 +0900

    code : 12919 solve

---
+++ src/main/kotlin/programmers/P12919.kt
@@ -0,0 +1,8 @@
+package programmers
+
+// 12919 서울에서 김서방 찾기
+// https://school.programmers.co.kr/learn/courses/30/lessons/12919
+class P12919 {
+    fun solution(seoul: Array<String>): String = "김서방은 ${seoul.indexOf("Kim")}에 있다"
+
+}
