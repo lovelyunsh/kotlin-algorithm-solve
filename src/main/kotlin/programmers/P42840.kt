c46d6e3e84cc8a10c049581c0825eb8b53b761bf
seunghwan <lovelyunsh@naver.com>
2023-12-13 11:02:05 +0900

    fix : 42480 .주석 추가

--- src/main/kotlin/programmers/P42840.kt
+++ src/main/kotlin/programmers/P42840.kt
@@ -1,5 +1,8 @@
 package programmers
 
+// 42840
+// https://school.programmers.co.kr/learn/courses/30/lessons/42840
+
 class P42840 {
     fun solution(answers: IntArray): IntArray {
         return usingMod(answers)
