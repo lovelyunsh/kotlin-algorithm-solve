b803c559094f46d5b145d327ea0380eebd07902e
seunghwan <lovelyunsh@naver.com>
2023-12-20 09:33:45 +0900

    fix : 12903 주석 추가

--- src/main/kotlin/programmers/P12903.kt
+++ src/main/kotlin/programmers/P12903.kt
@@ -1,6 +1,8 @@
 package programmers
 
+// 12903 가운데 글자 가져오기
+// https://school.programmers.co.kr/learn/courses/30/lessons/12903
 class P12903 {
-    fun solution(s: String): String = s.substring(s.length / 2 - if (s.length % 2 == 0) 1 else 0, s.length / 2 + 1)
+    fun solution(s: String) = s.substring(s.length / 2 - if (s.length % 2 == 0) 1 else 0, s.length / 2 + 1)
 
 }
