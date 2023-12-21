725f61f1c382d09fd8c4accedf9320fec330f1d8
seunghwan <lovelyunsh@naver.com>
2023-12-21 10:20:06 +0900

    code : 12917 solve

---
+++ src/main/kotlin/programmers/P12917.kt
@@ -0,0 +1,9 @@
+package programmers
+
+// 12917 문자열 내림차순으로 배치하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/12917
+class P12917 {
+    fun solution(s: String): String =
+        String(s.toCharArray().sortedDescending().toCharArray())
+
+}
