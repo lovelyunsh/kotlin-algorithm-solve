acdd5d30259ceecc5ca0f3d0d05ef2d3f3089a79
seunghwan <lovelyunsh@naver.com>
2023-12-26 09:34:05 +0900

    code : 12969 solve

--- src/main/kotlin/programmers/P12969.kt
+++ src/main/kotlin/programmers/P12969.kt
@@ -1,4 +1,10 @@
 package programmers
-
-class P12969 {
+// 12969 직사각형 별찍기
+// https://school.programmers.co.kr/learn/courses/30/lessons/12969
+fun main(args: Array<String>) {
+    val (a, b) = readln().split(' ').map(String::toInt)
+    var star = String(CharArray(a) { '*' })
+    for(i in 0 until b){
+        println(star)
+    }
 }
--- src/main/kotlin/programmers/test.kt
+++ src/main/kotlin/programmers/test.kt
@@ -2,7 +2,10 @@ package programmers
 
 fun main() {
 
-    var answer = "".matches(Regex("^[0-9]+$"))
-    print(answer)
+    var arr1 = arrayOf(arrayOf(1,2),arrayOf(2,3))
+    var arr2 = arrayOf(arrayOf(3,4),arrayOf(5,6))
+    var c = arr1 + arr2
+
+    print(c)
 
 }
